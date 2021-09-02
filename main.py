f53df521dd84cd2ac78af272f837a817a1bcc9dd
Michael Ortner <magpylib@gmail.com>
2021-09-02 09:49:34 +0200

    improve a lot

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ import numpy as np
 import magpylib as magpy
 import random
 from physics import getFT, collision_xy_cuboids
-from utility import create_bg_image
+from utility import create_bg_image, draw_player
 
 pygame.init()
 pygame.display.set_caption("Magnetic Engine")
 clock = pygame.time.Clock()
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-ORANGE = (255, 136, 0)
-GRAY = (200,200,200)
+
+
 
 #############################################################
 #############################################################
@@ -24,48 +19,35 @@ RESX, RESY = 960, 720
 display_force = True
 display_field_amp = True
 display_field_lines = True
-level = 3
-massfactorF = 1000        # translation inertia = 1/mass
-frictionF = 0.985         # translation friction
-massfactorT = 1000        # rotation inertia = 1/moment of inertia
+level = 2
+massfactorF = 800        # translation inertia = 1/density
+frictionF = 0.985       # translation friction
+massfactorT = 800        # rotation inertia = 1/moment of inertia (density)
 key_rot_vel = 5.5         # player key-rotation velocity 
-maxboing = 5              # allowed wall crashes
-player_size_x = 20
-player_size_y = 20
+player_size = (40, 30, 20)
+player_start_pos = (116, 628, 0)
 player_start_vel = (1,-1,0)
 nx, ny = 6, 6             # force computation discretization
 M0 = 50                   # overall magnetization amplitude
 #############################################################
 #############################################################
 
+
+# GAME INITIALIZATION #######################################
+
 # init target
 target_radius = int(RESX*0.05)
 target_pos = np.array((RESX-target_radius,target_radius,0))
 
 # init player
-start_pos_x = player_size_x + int(0.1*RESX)
-start_pos_y = int(0.9*RESY) - player_size_y
-player_pos = np.array((start_pos_x, start_pos_y, 0))
 player_vel = np.array(player_start_vel, dtype=float)
-player_vel_rot = 0
-player_mag = np.array((M0,0,0), dtype=float)
+player_vol = np.prod(player_size)/200
 player = magpy.magnet.Box(
-    magnetization = player_mag,
-    dimension = (2*player_size_x, 2*player_size_y, 20),
-    position = player_pos)
+    magnetization = (M0,0,0),
+    dimension = player_size,
+    position = player_start_pos)
 
-pp = np.array([
-    ( player_size_x, player_size_y,0), # Corner 1
-    ( player_size_x,-player_size_y,0), # Corner 2
-    (-player_size_x,-player_size_y,0), # Corner 3
-    (-player_size_x, player_size_y,0), # Corner 4
-    (-player_size_x/2,0,0),    # Arrow 1
-    ( player_size_x/2,0,0),    # Arrow 2
-    ( 0, player_size_y/4,0),   # Arrow 3
-    ( 0,-player_size_y/4,0)])   # Arrow 4
-
-# init obstactles
-#    obstacle setup
+# obstacle setups
 if level == 1:
     obst_dims = [[int(RESX/8)]*3]
     obst_possis = np.array([(RESX/2, RESY/2, 0)])
@@ -78,19 +60,9 @@ elif level == 2:
         (150, 375, 0), (450, 225, 0), (750,75,0),
         (375, 630, 0), (630, 525, 0), (900,300,0)])
     obst_mag = [(M0,0,0)]*6
-    obst_rot = [0,90,180,270]*4
-    obst_rot = random.sample(obst_rot, k=len(obst_possis))
-elif level ==3:
-    obst_dims = [[50,50,20]]*8
-    obst_possis = np.array([
-        (350,225,0), (350,475,0),
-        (450,200,0), (450,500,0),
-        (550,175,0), (550,525,0),
-        (650,150,0), (650,550,0)])
-    obst_mag = [(M0,0,0)]*8
-    obst_rot = [-45,45]*4
+    obst_rot = np.random.rand(6)*360
 
-#   create obst Magpylib Collection
+# init obstacles
 obst = magpy.Collection()
 for odim, opos, omag, orot in zip(obst_dims, obst_possis, obst_mag, obst_rot):
     obst + magpy.magnet.Box(
@@ -99,7 +71,7 @@ for odim, opos, omag, orot in zip(obst_dims, obst_possis, obst_mag, obst_rot):
         position = opos)
     obst[-1].rotate_from_angax(orot, 'z')
 
-# init background image
+# create/load background image
 if True:
     create_bg_image(RESX, RESY, display_field_amp, display_field_lines, obst)
 bg = pygame.image.load("bg.png")
@@ -108,8 +80,8 @@ bg = pygame.transform.flip(bg, False, True)
 
 # misc inits
 screen = pygame.display.set_mode((RESX, RESY), 0, 0)
-massfactorF /= player_size_x*player_size_y # scales with player size
-massfactorT /= player_size_x*player_size_y # scales with player size
+massfactorF /= player_vol # scales with player size
+massfactorT /= player_vol # scales with player size
 boings = 0            # boing counter
 game_active = True    # game active
 k_right = False       # right key
@@ -120,7 +92,7 @@ font = pygame.font.Font('freesansbold.ttf', 16)
 ############################################################
 while game_active:
 
-    # key events
+    # KEY EVENTS #######################################
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_active = False
@@ -143,57 +115,32 @@ while game_active:
         player.rotate_from_angax(-key_rot_vel, 'z')
 
 
-    # GAME PHYSICS ENGINE ####################################
-    play_pos = player.position
-    play_rot = player.orientation
-    play_mag = play_rot.apply(player_mag)
-
+    # GAME PHYSICS ENGINE ###############################
     # compute 3D force and torque
     F, T = getFT([obst], [player], player.position)
-    
     # avoid numerical cumulative problems
     F[2] = 0
     
-    # apply force
+    # apply force (inertia + friction)
     player_vel += F*massfactorF    # magneric acceleration
     player_vel *= frictionF        # air friction
-    play_pos += player_vel
-    player.position = play_pos
+    player.position += player_vel
     
-    # apply torque
+    # apply torque (no inertia = 100% friction)
     if not (k_right or k_left):
         player.rotate_from_angax(-T[2]*massfactorT*2000, 'z')
 
-    # GRAPHIC DISPLAY ##################################
+
+    # GRAPHIC DISPLAY ###################################
     screen.blit(bg, (0, 0))
 
-    # draw player
-    ppx = (play_rot.apply(pp) + play_pos)[:,:2]
-    pygame.draw.polygon(screen, GRAY, [ppx[0],ppx[1],ppx[2],ppx[3]])
-    pygame.draw.line(screen, GREEN, ppx[0], ppx[1], 5)
-    pygame.draw.line(screen, RED, ppx[2], ppx[3], 5)
-    pygame.draw.line(screen, BLACK, ppx[1], ppx[2], 3)
-    pygame.draw.line(screen, BLACK, ppx[3], ppx[0], 3)
-    pygame.draw.line(screen, BLACK, ppx[4],ppx[5], 3)
-    pygame.draw.line(screen, BLACK, ppx[5],ppx[6], 3)
-    pygame.draw.line(screen, BLACK, ppx[5],ppx[7], 3)
-
-
-    # display force vector
-    if display_force:
-        F_len = np.linalg.norm(F)
-        F_norm = F/F_len
-        pygame.draw.line(screen, ORANGE, play_pos[:2], play_pos[:2]+F_norm[:2]*(10+F_len*.1), 3)
+    draw_player(player, screen, display_force, F)
 
     # draw target location
-    pygame.draw.circle(screen, RED, target_pos[:2], target_radius, 6)
+    pygame.draw.circle(screen, (255, 0, 0), target_pos[:2], target_radius, 6)
 
-    # boing display
-    text = font.render(f'Boings = {boings}', True, BLACK, WHITE)
-    textRect = text.get_rect()
-    textRect.center = ( 50, 20)
-    screen.blit(text, textRect)
 
+    # GAME CONDITIONS ###################################
     # magnet collision
     collision = collision_xy_cuboids([player]*len(obst.sources), obst.sources)
     if isinstance(collision, int):
@@ -201,7 +148,8 @@ while game_active:
         print(collision)
         game_active = False
 
-    # crash into walls    
+    # wall boings
+    play_pos = player.position
     if (play_pos[0]<0) | (play_pos[0]>RESX):
         player_vel[0] = - player_vel[0]
         boings += 1
@@ -210,19 +158,15 @@ while game_active:
         player_vel[1] = - player_vel[1]
         boings += 1
         print('BOING !!!')
-    if boings>maxboing:
-        print('BOING + BOING + ... = CRASH !!!')
-        game_active = False
 
     # victory
     if np.linalg.norm(play_pos-target_pos)<target_radius/2:
         print('YOU WIN !!!')
         game_active = False
 
-    # UPDATE
-    pygame.display.flip()
 
-    # REFRESH
+    # UPDATE ##############################################
+    pygame.display.flip()
     clock.tick(120)
 
 pygame.quit()
--- utility.py
+++ utility.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pygame
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 def create_bg_image(
     RESX,
@@ -26,11 +28,59 @@ def create_bg_image(
     for ob in obst:
         orot = ob.orientation
         odx,ody,_ = ob.dimension
-        sq = np.array([(odx,ody,0),(odx,-ody,0),(-odx,-ody,0),(-odx,ody,0),(odx,ody,0)])/2
+        sq = np.array([(odx,ody,0),(odx,-ody,0),(-odx,-ody,0),(-odx,ody,0)])/2
         sq = orot.apply(sq) + ob.position
-        plt.plot(sq[:,0],sq[:,1], color='k', lw=20)
+        arr = np.array([(-odx/4,0,0), (odx/4,0,0), (0,ody/8,0), (odx/4,0,0), (0,-ody/8,0)])
+        arr = orot.apply(arr) + ob.position
+        #plt.plot(sq[:,0],sq[:,1], color='k', lw=20)
+        # Create a Rectangle patch
+        rect = patches.Polygon(sq[:,:2], linewidth=20, edgecolor='k', facecolor=(.3,.3,.3), zorder=9)
+        plt.plot(arr[:2,0], arr[:2,1], color='w', lw=20, zorder=10)
+        plt.plot(arr[2:,0], arr[2:,1], color='w', lw=20, zorder=10)
+        ax.add_patch(rect)
     # figure style
     ax.axis('off')
     ax.set(xlim=(0, RESX), ylim=(0,RESY))
     plt.tight_layout()
     plt.savefig('bg', dpi=10)
+
+
+def draw_player(player, screen, display_force, F):
+    """
+    draw player magnet on screen
+    """
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLACK = (0,0,0)
+    ORANGE = (255, 136, 0)
+    GRAY = (200,200,200)
+
+    # compute player points on screen
+    dimx, dimy, _ = player.dimension
+    pp = np.array([
+        ( dimx/2, dimy/2,0), # Corner 1
+        ( dimx/2,-dimy/2,0), # Corner 2
+        (-dimx/2,-dimy/2,0), # Corner 3
+        (-dimx/2, dimy/2,0), # Corner 4
+        (-dimx/4,0,0),    # Arrow 1
+        ( dimx/4,0,0),    # Arrow 2
+        ( 0, dimy/8,0),   # Arrow 3
+        ( 0,-dimy/8,0)])   # Arrow 4
+
+    # draw player
+    play_pos = player.position
+    ppx = (player.orientation.apply(pp) + play_pos)[:,:2]
+    pygame.draw.polygon(screen, GRAY, [ppx[0],ppx[1],ppx[2],ppx[3]])
+    pygame.draw.line(screen, GREEN, ppx[0], ppx[1], 5)
+    pygame.draw.line(screen, RED, ppx[2], ppx[3], 5)
+    pygame.draw.line(screen, BLACK, ppx[1], ppx[2], 3)
+    pygame.draw.line(screen, BLACK, ppx[3], ppx[0], 3)
+    pygame.draw.line(screen, BLACK, ppx[4],ppx[5], 3)
+    pygame.draw.line(screen, BLACK, ppx[5],ppx[6], 3)
+    pygame.draw.line(screen, BLACK, ppx[5],ppx[7], 3)
+
+    # display force vector
+    if display_force:
+        F_len = np.linalg.norm(F)
+        F_norm = F/F_len
+        pygame.draw.line(screen, ORANGE, play_pos[:2], play_pos[:2]+F_norm[:2]*(10+F_len*.1), 3)
